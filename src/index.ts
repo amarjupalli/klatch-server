290ba700566ade54e8082dae598f441ff40673b6
amarjupalli <ajupalli@turo.com>
2020-10-21 01:34:08 -0700

    chore: dist cleanup script and add logging for db connection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 
 async function main() {
-  const orm = await MikroORM.init(mikroOrmConfig);
-  await orm.getMigrator().up(); // run pre migrations
+  try {
+    const orm = await MikroORM.init(mikroOrmConfig);
+    await orm.getMigrator().up(); // run pre migrations
+  } catch (error) {
+    console.error("Database connection failed", error);
+  }
 }
 
 main().catch((err) => console.log("ERROR", err));
