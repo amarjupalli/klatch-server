1e37a8aa4c938f27ecabf1f6c60bed731bad4609
amarjupalli <ajupalli@turo.com>
2020-10-22 08:28:46 -0700

    chore: update type for Orm context

--- src/resolvers/types.ts
+++ src/resolvers/types.ts
@@ -1,5 +1,8 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+import { Request, Response } from "express";
 
 export type ORMContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  req: Request;
+  res: Response;
 };
