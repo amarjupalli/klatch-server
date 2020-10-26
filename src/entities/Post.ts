a9b706f076a3d5d8989070d92d1564019f60fb8e
amarjupalli <ajupalli@turo.com>
2020-10-25 17:27:21 -0700

    feat: add new fields, 1->Many and Many->1 rels b/w User and Post entities

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -5,14 +5,35 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
 } from "typeorm";
 import { Field, ObjectType } from "type-graphql";
+import { User } from "./User";
 @ObjectType()
 @Entity()
 export class Post extends BaseEntity {
   @Field()
   @PrimaryGeneratedColumn()
   id!: number;
+
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  points!: number;
+
+  @Field()
+  @Column()
+  creatorId: number;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  creator: User;
 
   @Field(() => String)
   @CreateDateColumn()
@@ -21,8 +42,4 @@ export class Post extends BaseEntity {
   @Field(() => String)
   @UpdateDateColumn()
   updatedAt: Date;
-
-  @Field()
-  @Column({ type: "text" })
-  title!: string;
 }
